9da8d1ed28f7959a86f06da085b50b38975f212b
g-stro <gstroud@live.co.uk>
2022-05-15 17:53:18 +0100

    Added functionality for drawing circles to the screen

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"image/color"
 	"log"
+	"math"
 
 	"github.com/hajimehoshi/ebiten/v2"
 )
@@ -10,18 +11,18 @@ import (
 type Game struct{}
 
 type player struct {
-	x   float64
-	y   float64
-	img *ebiten.Image
+	x      float64
+	y      float64
+	radius float64
+	img    *ebiten.Image
+	clr    color.Color
 }
 
 var p player
 
 // Update the game state
 func (g *Game) Update() error {
-	p.img.Fill(color.RGBA{0, 0xff, 0, 0xff})
-
-	// Keyboard input
+	// Keyboard input for player movement
 	if ebiten.IsKeyPressed(ebiten.KeyUp) {
 		p.y--
 	}
@@ -38,12 +39,27 @@ func (g *Game) Update() error {
 	return nil
 }
 
-// Render to the screen
+// drawCircle draws a circle to the screen
+func drawCircle(screen *ebiten.Image, p player) {
+	angle := math.Acos(1 - 1/p.radius)
+	// iterate over each angle until the circle is complete
+	for a := float64(0); a <= 360; a += angle {
+		// Delta x and y values
+		xd := p.radius * math.Cos(a)
+		yd := p.radius * math.Sin(a)
+		// draw the coordinates to the screen
+		x := int(math.Round(p.x + xd))
+		y := int(math.Round(p.y + yd))
+		screen.Set(x, y, p.clr)
+	}
+}
+
+// Draw renders graphics to the screen
 func (g *Game) Draw(screen *ebiten.Image) {
 	opts := &ebiten.DrawImageOptions{}
 	// Add the Translate effect to the option struct
 	opts.GeoM.Translate(p.x, p.y)
-	screen.DrawImage(p.img, opts)
+	drawCircle(screen, p)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -54,7 +70,7 @@ func main() {
 	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowTitle("KRAK ORRRRRRK!")
 	// Create the player
-	p = player{64, 48, ebiten.NewImage(32, 32)}
+	p = player{64, 48, 16, ebiten.NewImage(32, 32), color.RGBA{0, 0xff, 0, 0xff}}
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
 	}
